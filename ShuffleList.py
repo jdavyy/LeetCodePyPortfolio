c3d2a3da4e92e5f9f98336eed53194163af49528
jdavyy <132872260+jdavyy@users.noreply.github.com>
2023-05-08 00:17:33 -0700

    Update ShuffleList.py

--- ShuffleList.py
+++ ShuffleList.py
@@ -18,6 +18,34 @@ class Solution(object):
         
         return shuffled
     
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        if len(nums) == 0:
+            return nums
+        else: 
+            stack = []
+            stack.append(0)
+            x = 0
+            for item in nums:
+                print(x)
+                a = stack[x] + nums[x]
+                stack.append(a)
+                x += 1
+        
+        stack.pop(0)
+        return stack
+    
+nums = [1,2,3,4]
+nums2 = [1,1,1,1,1]
+nums3 = [3,1,2,10,1]
+print(solution.runningSum(nums))
+print(solution.runningSum(nums2))
+print(solution.runningSum(nums3))
+    
 numba = [1,2,3,4,5,6,7,8]
 nn = 4
 solution = Solution()
